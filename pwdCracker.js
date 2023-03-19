b652e66aeba540ac1f507d09067380be750bc9e7
iNFiENiTE-World <infienite@gmail.com>
2023-03-19 23:32:10 +0800

    Implement basic functionalities to keep password

--- pwdCracker.js
+++ pwdCracker.js
@@ -2,15 +2,29 @@ import { hash } from 'bcrypt'
 import { writeFile, readFile } from 'node:fs/promises'
 
 const keepPassword = async (urlDomain, pwd) => {
+  const hashedPwd = await hash(pwd, 10)
+  const data = await getData()
+  data.push({
+    urlDomain,
+    hash: hashedPwd
+  })
 
+  await writeFile('./stored_hash.json', data)
+}
+
+const getData = async () => {
+  return JSON.parse(await readFile('./stored_hash.json'))
 }
 
 const searchDomain = async (urlDomain) => {
-
+  const allUrlDomain = await getUrlDomain()
+  return allUrlDomain.findIndex((url) => {
+    return url === urlDomain
+  })
 }
 
-const listUrlDomain = async () => {
-  const data = JSON.parse(await readFile('./stored_hash.json'))
+const getUrlDomain = async () => {
+  const data = await getData()
   return data.map((pwd) => {
     return pwd.urlDomain
   })
@@ -20,4 +34,9 @@ const tellPassword = async () => {
 
 }
 
-listUrlDomain()
+export {
+  keepPassword,
+  searchDomain,
+  getUrlDomain,
+  tellPassword
+}
