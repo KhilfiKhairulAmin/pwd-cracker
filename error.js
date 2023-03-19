c15d9b7e7022ac572f3fd84a158eb4d189c9b0c8
iNFiENiTE-World <infienite@gmail.com>
2023-03-19 19:27:56 +0800

    Initialized new error handler

--- error.js
+++ error.js
@@ -5,4 +5,9 @@ class InterfaceError extends Error {
   }
 }
 
-export default InterfaceError
+const InterfaceNotStartedError = new InterfaceError('Interface is not started. Please start the interface to use it.')
+
+export {
+  InterfaceError,
+  InterfaceNotStartedError
+}
