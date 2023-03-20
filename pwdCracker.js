7921c4a40870ae1ec3a864efeb051d2146c51e13
iNFiENiTE-World <infienite@gmail.com>
2023-03-20 19:58:18 +0800

    Upgraded to Class-Based Implementation

--- pwdCracker.js
+++ pwdCracker.js
@@ -1,43 +1,49 @@
 import { hash } from 'bcrypt'
 import { writeFile, readFile } from 'node:fs/promises'
 
-const keepPassword = async (urlDomain, pwd) => {
-  const hashedPwd = await hash(pwd, 10)
-  const data = await getData()
-
-  data.push({
-    urlDomain,
-    hash: hashedPwd
-  })
-
-  await writeFile('./stored_hash.json', JSON.stringify(data))
-}
-
-const getData = async () => {
-  try {
-    const data = JSON.parse(await readFile('./stored_hash.json'))
-    return data
-  } catch (err) {
-    return []
+class PwdCracker {
+  constructor () {
+    
   }
-}
 
-const searchDomain = async (urlDomain) => {
-  const allUrlDomain = await getUrlDomain()
-  return allUrlDomain.findIndex((url) => {
-    return url === urlDomain
-  })
-}
-
-const getUrlDomain = async () => {
-  const data = await getData()
-  return data.map((pwd) => {
-    return pwd.urlDomain
-  })
-}
-
-const tellPassword = async () => {
-
+  async keepPassword (urlDomain, pwd) {
+    const hashedPwd = await hash(pwd, 10)
+    const data = await getData()
+  
+    data.push({
+      urlDomain,
+      hash: hashedPwd
+    })
+  
+    await writeFile('./stored_hash.json', JSON.stringify(data))
+  }
+  
+  async getData () {
+    try {
+      const data = JSON.parse(await readFile('./stored_hash.json'))
+      return data
+    } catch (err) {
+      return []
+    }
+  }
+  
+  async searchDomain (urlDomain) {
+    const allUrlDomain = await getUrlDomain()
+    return allUrlDomain.findIndex((url) => {
+      return url === urlDomain
+    })
+  }
+  
+  async getUrlDomain () {
+    const data = await getData()
+    return data.map((pwd) => {
+      return pwd.urlDomain
+    })
+  }
+  
+  async tellPassword () {
+  
+  }
 }
 
 export {
