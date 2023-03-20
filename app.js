9ab356008ce0a203bf13efab03727812c4bb77db
iNFiENiTE-World <infienite@gmail.com>
2023-03-20 22:40:48 +0800

    Implemented for tellPassword function

--- app.js
+++ app.js
@@ -18,10 +18,21 @@ io.start()
 
 io.chat('Welcome to PWD Cracker')
 
+// Code for KEEP PASSWORD
+// const urlDomain = await io.prompt('URL Domain: ')
+// const pwd = await io.prompt('Password: ')
+
+// await pwdCracker.keepPassword(urlDomain, pwd)
+
+// Code for TELL PASSWORD
 const urlDomain = await io.prompt('URL Domain: ')
 const pwd = await io.prompt('Password: ')
 
-await pwdCracker.keepPassword(urlDomain, pwd)
+if (await pwdCracker.tellPassword(urlDomain, pwd)) {
+  console.log('Success')
+} else {
+  console.log('URL doesn\'t exist OR Password is false')
+}
 
 io.stop()
 currentState = state.OFF
