1d5d71b2c2125373d718fe294bbb1e4bd3dd6588
iNFiENiTE-World <infienite@gmail.com>
2023-03-21 18:20:25 +0800

    Tester for pwdCracker

--- app.js
+++ app.js
@@ -26,13 +26,8 @@ io.chat('Welcome to PWD Cracker')
 
 // Code for TELL PASSWORD
 const urlDomain = await io.prompt('URL Domain: ')
-const pwd = await io.prompt('Password: ')
 
-if (await pwdCracker.tellPassword(urlDomain, pwd)) {
-  console.log('Success')
-} else {
-  console.log('URL doesn\'t exist OR Password is false')
-}
+console.log(await pwdCracker.tellPassword(urlDomain))
 
 io.stop()
 currentState = state.OFF
