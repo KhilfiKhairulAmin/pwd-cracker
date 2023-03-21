00b5c7e6b514631a927aa6432116f3ca17c4b8f4
iNFiENiTE-World <infienite@gmail.com>
2023-03-21 18:50:57 +0800

    Implemented basic interface

--- app.js
+++ app.js
@@ -11,23 +11,41 @@ const state = {
 
 let currentState = state.ON
 
-if (!currentState) process.exit(0)
-
 const io = new TerminalInterface()
 io.start()
 
 io.chat('Welcome to PWD Cracker')
 
-// Code for KEEP PASSWORD
-// const urlDomain = await io.prompt('URL Domain: ')
-// const pwd = await io.prompt('Password: ')
+while (currentState) {
+  const command = Number(await io.prompt('[0] Keep password    [1] Tell password    [2] Quit    \n'))
 
-// await pwdCracker.keepPassword(urlDomain, pwd)
-
-// Code for TELL PASSWORD
-const urlDomain = await io.prompt('URL Domain: ')
-
-console.log(await pwdCracker.tellPassword(urlDomain))
+  switch (command) {
+    case 0: {
+      // Code for KEEP PASSWORD interface
+      io.chat('Please enter the following information')
+      const urlDomain = await io.prompt('URL Domain: ')
+      const pwd = await io.prompt('Password: ')
+      await pwdCracker.keepPassword(urlDomain, pwd)
+      io.chat('Success!')
+      break
+    }
+    case 1: {
+      // Code for TELL PASSWORD inteface
+      io.chat('Please enter the following information')
+      const urlDomain = await io.prompt('URL Domain: ')
+      const success = await pwdCracker.tellPassword(urlDomain)
+      if (success) {
+        io.chat('Success! Password: ' + success)
+      }
+      break
+    }
+    case 2: {
+      currentState = state.OFF
+      continue
+    }
+  }
+  io.chat('\n')
+}
 
 io.stop()
-currentState = state.OFF
+process.exit(0)
