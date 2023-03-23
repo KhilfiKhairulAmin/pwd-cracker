20bc399b65d30f666c6e9ac7f05d1da2db10202d
iNFiENiTE-World <infienite@gmail.com>
2023-03-23 21:11:22 +0800

    Documented

--- io.js
+++ io.js
@@ -1,33 +1,63 @@
-import { createInterface } from 'readline/promises'
+import { createInterface } from 'node:readline/promises'
 import { stdin as input, stdout as output } from 'node:process'
 
+/**
+ * Error object used in `TerminalInterface` class context
+ */
 class TerminalInterfaceError extends Error {
+  /**
+   * @param {String} message Explanation of the error
+   */
   constructor (message) {
     super(message)
     this.name = 'TerminalInterfaceError'
   }
 }
 
+/**
+ * A `TerminalInterface` instance must be started before using it
+ */
 const TerminalInterfaceNotStartedError = new TerminalInterfaceError('Terminal interface is not started. Please start the interface to use it.')
 
+/**
+ * **Handles input and output** process in the command line. The class provides minimal functionalities for ease of use to gain input or/and display output.
+ * This class collects input from `stdin` and display output through `stdout`.
+ */
 class TerminalInterface {
+  /**
+   * Stores `Interface` object from `node:readline/promises` module
+   */
   #io
+  /**
+   * Stores current status of terminal
+   */
   #on
 
   constructor () {
-    this.#io = createInterface({ input, output })
     this.#on = false
   }
 
+  /**
+   * Enable interface to collect input and display output
+   */
   start () {
+    this.#io = createInterface({ input, output })
     this.#on = true
   }
 
+  /**
+   * Disable interface to collect input and display output
+   */
   stop () {
     this.#on = false
     this.#io.close()
   }
 
+  /**
+   * Collect input in command line
+   * @param {String} question Describe data to be given by user
+   * @returns {String} Data given
+   */
   async prompt (question) {
     try {
       if (!this.#isOn()) {
@@ -40,6 +70,10 @@ class TerminalInterface {
     }
   }
 
+  /**
+   * Say something to your user
+   * @param {String} message Displayed to your user
+   */
   chat (message) {
     try {
       if (!this.#isOn()) {
@@ -52,10 +86,18 @@ class TerminalInterface {
     }
   }
 
+  /**
+   * Return the current status of terminal
+   * @returns {Boolean}
+   */
   #isOn () {
     return this.#on
   }
 
+  /**
+   * Handles error inside the class
+   * @param {TerminalInterfaceError} err Error that occured in `TerminalInterface` context
+   */
   #errorHandler (err) {
     console.error(err.stack)
     process.exit(1)
